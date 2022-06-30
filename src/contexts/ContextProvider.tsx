b9785d1797d2f39bb1059c787ed3402e56bcb4b5
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-06-30 15:58:31 -0300

    Navbar component
    Done the navbar component with his buttons and the system of profile section
    
    Some new types for context api
    
    An eventListener inside a useEffect hook for we going to have the screen size and after remove the eventListener

--- src/contexts/ContextProvider.tsx
+++ src/contexts/ContextProvider.tsx
@@ -6,12 +6,43 @@ type ContextProviderProps = {
 
 type StateContextType = { 
     activeMenu: boolean,
-    setActiveMenu: (newState: boolean) => void
+    setActiveMenu: (newState: boolean) => void,
+
+    isClicked: {
+        chat: boolean,
+        cart: boolean,
+        userProfile: boolean,
+        notification: boolean
+    },
+    setIsClicked: (newState: {
+        chat: boolean,
+        cart: boolean,
+        userProfile: boolean,
+        notification: boolean
+    }) => void,
+
+    handleClick: (clicked: string) => void,
+
+    screenSize: number,
+    setScreenSize: (newState: number) => void,
 }
 
 const stateInicialValue = {
     activeMenu: false,
-    setActiveMenu: () => {}
+    setActiveMenu: () => {},
+
+    isClicked: {
+        chat: false,
+        cart: false,
+        userProfile: false,
+        notification: false
+    },
+    setIsClicked: () => {},
+
+    handleClick: () => {},
+
+    screenSize: 0,
+    setScreenSize: () => {}
 }
 
 const StateContext = createContext<StateContextType>(stateInicialValue);
@@ -20,17 +51,28 @@ const initialState = {
     chat: false,
     cart: false,
     userProfile: false,
-    notification: false
+    notification: false,
 }
 
 export const ContextProvider = ({ children }: ContextProviderProps) => {
     const [activeMenu, setActiveMenu] = useState(true)
+    const [isClicked, setIsClicked] = useState(initialState)
+    const [screenSize, setScreenSize] = useState(0)
+
+    const handleClick = (clicked: string) => {
+        setIsClicked({...initialState, [clicked]: true})
+    }
 
     return(
         <StateContext.Provider
             value={{
                 activeMenu,
-                setActiveMenu
+                setActiveMenu,
+                isClicked,
+                setIsClicked,
+                handleClick,
+                screenSize, 
+                setScreenSize
             }}
         >
             {children}
