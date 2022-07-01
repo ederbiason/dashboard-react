6137b6ecb964afa61a7bcd0873c31bf10fcb389f
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-07-01 15:44:57 -0300

    Stacked chart
    done the stacked chart with the data
    but I need to fix the primaryXAxis type, it's causing error

--- src/components/Charts/Stacked.tsx
+++ src/components/Charts/Stacked.tsx
@@ -1,6 +1,29 @@
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts'
 
-export default function Stacked() {
+import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis} from '../../data/dummy'
+
+type StackedProps = {
+  width: string;
+  height: string;
+}
+
+export default function Stacked({width, height}: StackedProps) {
+
   return (
-    <div>Stacked</div>
+    <ChartComponent
+      width={width}
+      height={height}
+      id="charts"
+      // primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      chartArea={{border: {width: 0}}}
+      tooltip={{enable: true}}
+      legendSettings={{background: 'white'}}
+    >
+      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+      </SeriesCollectionDirective>
+    </ChartComponent>
   )
 }
--- src/pages/Ecommerce.tsx
+++ src/pages/Ecommerce.tsx
@@ -124,6 +124,13 @@ export default function Ecommerce() {
                     />
                   </div>
                 </div>
+
+                <div>
+                  <Stacked 
+                    width="320px"
+                    height="360px"
+                  />
+                </div>
               </div>
           </div>
       </div>
