9dc90e28566304b891a922dc15825df3ccffc20e
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-07-06 15:35:40 -0300

    Pie chart
    Done the pie chart component with the same structure of the others

--- src/components/Charts/Pie.tsx
+++ src/components/Charts/Pie.tsx
@@ -1,6 +1,52 @@
+import { AccumulationChartComponent, AccumulationSeriesCollectionDirective, AccumulationSeriesDirective, AccumulationLegend, PieSeries, AccumulationDataLabel, Inject, AccumulationTooltip } from "@syncfusion/ej2-react-charts"
+import { ReactNode } from "react";
 
-export default function Pie() {
+import { useStateContext } from "../../contexts/ContextProvider"
+
+type PieProps = {
+  id: string;
+  data: { x: string; y: number; text: string; }[];
+  legendVisibility: boolean;
+  height: string;
+}
+
+export default function Pie({id, data, legendVisibility, height}: PieProps) {
+  const { currentMode } = useStateContext()
+  
   return (
-    <div>Pie</div>
+    <AccumulationChartComponent
+      id={id}
+      legendSettings={{visible: legendVisibility, background: 'white'}}
+      height={height}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+      tooltip={{ enable: true }}
+    >
+      <Inject services={[AccumulationLegend, PieSeries, AccumulationDataLabel, AccumulationTooltip]} />
+
+      <AccumulationSeriesCollectionDirective>
+        <AccumulationSeriesDirective 
+          name="Sales"
+          dataSource={data}
+          xName="x"
+          yName="y"
+          innerRadius="40%"
+          startAngle={0}
+          endAngle={360}
+          radius="70%"
+          explode
+          explodeOffset="10%"
+          explodeIndex={2}
+          dataLabel={{
+            visible: true,
+            name: 'text',
+            position: 'Inside',
+            font: {
+              fontWeight: '600',
+              color: '#fff',
+            }
+          }}
+        />
+      </AccumulationSeriesCollectionDirective>
+    </AccumulationChartComponent>
   )
 }
