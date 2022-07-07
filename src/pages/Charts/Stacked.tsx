b5e354fe68b427c5ac5db83addd01e4caa2a8208
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-07-07 15:07:03 -0300

    Stacked chart
    The same stacked chart used in Employee component

--- src/pages/Charts/Stacked.tsx
+++ src/pages/Charts/Stacked.tsx
@@ -1,6 +1,12 @@
+import { ChartsHeader, Stacked as StackedChart } from "../../components";
 
 export default function Stacked() {
   return (
-    <div>Stacked</div>
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <ChartsHeader category="Stacked" title="Revenue Breakdown" />
+      <div className="w-full">
+        <StackedChart height='' width='' />
+      </div>
+    </div>
   )
 }
